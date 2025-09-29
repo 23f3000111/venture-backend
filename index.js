61ad6fa2abcef0c6c19aca09bcb5803ea5d928e6
Rishikesh Mahto <mahtorishikesh93@gmail.com>
2025-09-29 12:01:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ const app = express();
 const cors = require("cors");
 app.use(cors({ origin: "*" }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") return res.sendStatus(204);
-  next();
-});
 
 app.use(express.json());
 const { intializeDatabase } = require("./db/db.connect")
@@ -31,3 +24,4 @@ app.get("/venture", async (req, res) => {
 });
 
 module.exports = app;
+
